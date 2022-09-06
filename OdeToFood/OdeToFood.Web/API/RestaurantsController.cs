f68ae6d725937d3a46069e0981baafb54231e42f
PIT-LPT-057\JoshMorton <josh.morton@propeltech.co.uk>
2022-09-06 16:40:17 +0100

    Configured routes, added WebApi support

---
+++ OdeToFood/OdeToFood.Web/API/RestaurantsController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace OdeToFood.Web.API
+{
+    public class RestaurantsController : ApiController
+    {
+        //GET
+        public string Get()
+        {
+            return "Hello World";
+        }
+    }
+}
---
+++ OdeToFood/OdeToFood.Web/App_Start/ContainerConfig.cs
@@ -0,0 +1,36 @@
+﻿using Autofac;
+using Autofac.Integration.Mvc;
+using OdeToFood.Data.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OdeToFood.Web
+{
+    public class ContainerConfig
+    {
+        internal static void RegisterContainer()
+        {
+            var builder = new ContainerBuilder();
+
+            //sets RegisterControllers to scan our MvcApplication Assembly to find and register all our controllers.
+            builder.RegisterControllers(typeof(MvcApplication).Assembly);
+            //Tells our builder to know about the InMemeryRestaurtantData and use it when someone needs something that implemenets IRestaurantData
+            builder.RegisterType<InMemoryRestaurantData>()
+                .As<IRestaurantData>()
+                .SingleInstance(); //This will never work if we have multiple users.
+
+            //AutoFac now knows about that type, and use that type whenever somebody needs something about IRestaurantData, this also means that down the line
+            //we can easily switch the reference to our database so that it knows how to handle the data we will be passing.
+            //To begin with, we want a singleinstance because when we start adding data in, we want it all to be inside of one Object
+
+            var container = builder.Build();
+            //Then we create out actual container using the Build method from Autofac on our ContainerBuilder
+
+            //This uses MVC's Dependency resolver to set itself to our AutofacDependencyResolver and passes in our Container
+            DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
+        }
+    }
+}
