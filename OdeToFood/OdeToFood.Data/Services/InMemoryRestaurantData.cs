b89906b735d2415ffda76d12b425ac73cf519b92
PIT-LPT-057\JoshMorton <josh.morton@propeltech.co.uk>
2022-09-08 13:00:27 +0100

    Building a form for submitting new Restaurant entries

--- OdeToFood/OdeToFood.Data/Services/InMemoryRestaurantData.cs
+++ OdeToFood/OdeToFood.Data/Services/InMemoryRestaurantData.cs
@@ -13,10 +13,17 @@ namespace OdeToFood.Data.Services
             restaurants = new List<Restaurant>()
             {
                 new Restaurant{ Id = 1, Name = "Scott's Pizza", Cuisine = CuisineType.Italian },
-                new Restaurant{ Id = 1, Name = "Terisguels", Cuisine = CuisineType.French },
-                new Restaurant{ Id = 1, Name = "Mango Grove", Cuisine = CuisineType.Indian }
+                new Restaurant{ Id = 2, Name = "Terisguels", Cuisine = CuisineType.French },
+                new Restaurant{ Id = 3, Name = "Mango Grove", Cuisine = CuisineType.Indian }
             };
         }
+
+        public Restaurant Get(int id)
+        {
+            //Search inside the DB for an entry that's ID matches the ID parameter passed, or return the first result.
+            return restaurants.FirstOrDefault(r => r.Id == id);
+        }
+
         public IEnumerable<Restaurant> GetAll()
         {
             return restaurants.OrderBy(r => r.Name);
